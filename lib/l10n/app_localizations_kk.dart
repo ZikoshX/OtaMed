f05ff1b1d68fe9bf7bfda24228891035884fbde7
Zikosh <150950933+ZikoshX@users.noreply.github.com>
2025-05-02 01:06:09 +0500

    Update app_localizations_kk.dart

--- lib/l10n/app_localizations_kk.dart
+++ lib/l10n/app_localizations_kk.dart
@@ -31,4 +31,142 @@ class AppLocalizationsKk extends AppLocalizations {
 
   @override
   String get logout => 'Шығу';
+
+  @override
+  String get favorites => 'Тандаулылар';
+
+  @override
+  String get ai_chat => 'ЖИ чат';
+
+  @override
+  String get search => 'іздеу';
+
+  @override
+  String get clinics => 'Клиникалар';
+
+  @override
+  String get new_chat => 'Жаңа чат';
+
+  @override
+  String get name => 'Аты';
+
+  @override
+  String get description => 'Сипаттамалар';
+
+  @override
+  String get category => 'Категория';
+
+  @override
+  String get address => 'Мекен-жай';
+
+  @override
+  String get phone => 'Телефон';
+
+  @override
+  String get other => 'Қосымша ақпараттар';
+
+  @override
+  String get type_operations => 'Операция түрлері';
+
+  @override
+  String get plastic_surgery => 'Пластикалық хирургия клиникасы';
+
+  @override
+  String get orthopedic => 'Ортопедия клиникалар';
+
+  @override
+  String get oncological => 'Онкологиялық клиникалар';
+
+  @override
+  String get neurosurgery => 'Нейрохирургия';
+
+  @override
+  String get type_treatment => 'Емдеу түрлері';
+
+  @override
+  String get ent => 'ЛОР клиникалар';
+
+  @override
+  String get gastroenterology => 'Гастроэнтерология клиникалары';
+
+  @override
+  String get urology => 'Урологиялық клиникалар';
+
+  @override
+  String get ophthalmology => 'Офтальмологиялық клиникалар';
+
+  @override
+  String get dermatology => 'Дерматологиялық клиникалар';
+
+  @override
+  String get physical => 'Физикалық терапия және оңалту клиникалары';
+
+  @override
+  String get us => 'Біз туралы';
+
+  @override
+  String get usAnswer => '«Ota Med» — Еуропа мен Азиядағы ең жақсы клиникалар мен процедураларды табуға көмектесетін қосымша. Біз ашық дереккөздерден ақпарат жинап, оны ыңғайлы форматта ұсынамыз, осылайша сіз саналы шешім қабылдай аласыз.';
+
+  @override
+  String get searchHelp => 'Іздеу және сүзгілеу';
+
+  @override
+  String get searchAnswer => 'Клиникаларды іздеу:\n- Ем түрі бойынша\n- Ел мен қала бойынша.';
+
+  @override
+  String get ai_help => 'Ақылды AI көмекшісі';
+
+  @override
+  String get ai_answer => 'Біздің AI чат:\n- Сұрақтарға жауап береді\n- Дұрыс клиниканы таңдауға көмектеседі\n- Емханаларды салыстырады';
+
+  @override
+  String get contact => 'Клиника контактілері';
+
+  @override
+  String get contact_answer => 'Қолданбада мәселе болса, ресми поштамызға жазыңыз! \notamed2025@gmail.com';
+
+  @override
+  String get important => 'Маңызды!!!! \nЖауапкершіліктен бас тарту';
+
+  @override
+  String get impor_answer => 'Ұсынылған ақпарат ашық дереккөздерден жиналған. Пайдаланушылар осы деректер негізінде шешім қабылдауға өздері жауапты.';
+
+  @override
+  String get faq => 'Жиі қойылатын сұрақтар (FAQ)';
+
+  @override
+  String get procedure => 'Процедура түрі бойынша қалай табуға болады?';
+
+  @override
+  String get procedure_answer => 'Іздеу бөлімінде қажетті қызметтерді ұсынатын клиникаларды сүзгілеу үшін процедура түрін таңдауға болады.';
+
+  @override
+  String get rating => 'Клиника рейтингтері нені білдіреді?';
+
+  @override
+  String get rating_answer => 'Клиниканың рейтингі науқастардың пікірлеріне және қызметтердің жалпы сапасына негізделген. Рейтинг жоғары болған сайын клиника көбірек ұсынылады.';
+
+  @override
+  String get number => 'Клиникамен қалай байланысуға болады?';
+
+  @override
+  String get number_answer => 'Әрбір клиниканың бетінде байланыс деректері, мысалы, телефон нөмірлері, электрондық пошта мекенжайлары және әлеуметтік желі сілтемелері көрсетілген.';
+
+  @override
+  String get correct => 'Клиника ақпаратының дұрыстығын қалай тексеруге болады?';
+
+  @override
+  String get correct_answer => 'Барлық деректер ашық көздерден, мысалы, клиникалардың ресми сайттарынан, медициналық рейтингтік платформалардан және пайдаланушылардың пікірлерінен жиналады.';
+
+  @override
+  String get filter => 'Сүзгілерді қалай қолдану керек?';
+
+  @override
+  String get filter_answer => 'Ел, қала, ем түрі бойынша іздеуді реттеңіз.';
+
+  @override
+  String get incorrect => 'Қосымшада қиындықтар туындаса не істеу керек?';
+
+  @override
+  String get incorrect_answer => 'Егер сұрақтарыңыз немесе мәселелеріңіз болса, парақшада көрсетілген байланыс формасы немесе электрондық пошта арқылы бізге хабарласыңыз.';
 }
