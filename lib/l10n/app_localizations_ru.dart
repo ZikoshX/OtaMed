a00c0b2bbb6224bd6a86155dead86787a292060a
Zikosh <150950933+ZikoshX@users.noreply.github.com>
2025-05-02 01:06:32 +0500

    Update app_localizations_ru.dart

--- lib/l10n/app_localizations_ru.dart
+++ lib/l10n/app_localizations_ru.dart
@@ -31,4 +31,142 @@ class AppLocalizationsRu extends AppLocalizations {
 
   @override
   String get logout => 'Выйти';
+
+  @override
+  String get favorites => 'Избранное';
+
+  @override
+  String get ai_chat => 'ИИ чат';
+
+  @override
+  String get search => 'искать';
+
+  @override
+  String get clinics => 'Клиники';
+
+  @override
+  String get new_chat => 'Новый чат';
+
+  @override
+  String get name => 'Имя';
+
+  @override
+  String get description => 'Описание';
+
+  @override
+  String get category => 'Категория';
+
+  @override
+  String get address => 'Адрес';
+
+  @override
+  String get phone => 'Телефон';
+
+  @override
+  String get other => 'Другие информации';
+
+  @override
+  String get type_operations => 'Виды операции';
+
+  @override
+  String get plastic_surgery => 'Клиника пластической хирургии';
+
+  @override
+  String get orthopedic => 'Ортопедическая клиника';
+
+  @override
+  String get oncological => 'Онкологические клиники';
+
+  @override
+  String get neurosurgery => 'Нейрохирургия';
+
+  @override
+  String get type_treatment => 'Виды лечение';
+
+  @override
+  String get ent => 'ЛОР-клиники';
+
+  @override
+  String get gastroenterology => 'Гастроэнтерологические клиники';
+
+  @override
+  String get urology => 'Урологические клиники';
+
+  @override
+  String get ophthalmology => 'Офтальмологические клиники';
+
+  @override
+  String get dermatology => 'Дерматологические клиники';
+
+  @override
+  String get physical => 'Клиника физиотерапии и реабилитации';
+
+  @override
+  String get us => 'О нас';
+
+  @override
+  String get usAnswer => '«Ota Med» — это приложение, которое помогает найти лучшие клиники и процедуры в Европе и Азии. Мы собираем данные из открытых источников и предоставляем их в удобном формате, чтобы вы могли легко принять обоснованное решение.';
+
+  @override
+  String get searchHelp => 'Поиск и фильтрация';
+
+  @override
+  String get searchAnswer => 'Вы можете искать клиники по:\n- Типу процедуры или лечения\n- Стране и городу.';
+
+  @override
+  String get ai_help => 'Умный AI помощник';
+
+  @override
+  String get ai_answer => 'Наш встроенный AI чат:\n- Отвечает на вопросы\n- Помогает выбрать клинику\n- Сравнивает варианты';
+
+  @override
+  String get contact => 'Контакты клиник';
+
+  @override
+  String get contact_answer => 'Если у вас есть вопросы, пишите нам на официальную почту! \notamed2025@gmail.com';
+
+  @override
+  String get important => 'ВАЖНО!!!! \nОтказ от ответственности';
+
+  @override
+  String get impor_answer => 'Предоставленная информация собрана из открытых источников. Пользователи сами несут ответственность за принятие решений на основе этих данных.';
+
+  @override
+  String get faq => 'Часто задаваемые вопросы (FAQ)';
+
+  @override
+  String get procedure => 'Как найти клинику по типу процедуры?';
+
+  @override
+  String get procedure_answer => 'В разделе поиска вы можете выбрать тип процедуры, чтобы отфильтровать клиники, предоставляющие соответствующие услуги.';
+
+  @override
+  String get rating => 'Что означают рейтинги клиник?';
+
+  @override
+  String get rating_answer => 'Рейтинг клиники основан на отзывах пациентов и общем качестве услуг. Чем выше рейтинг, тем более рекомендуется клиника.';
+
+  @override
+  String get number => 'Как связаться с клиникой?';
+
+  @override
+  String get number_answer => 'На странице каждой клиники указаны контактные данные, такие как номера телефонов, адреса электронной почты и ссылки на социальные сети для связи с клиникой.';
+
+  @override
+  String get correct => 'Как проверить информацию о клинике?';
+
+  @override
+  String get correct_answer => 'Все данные собираются из открытых источников, таких как официальные сайты клиник, медицинские рейтинговые платформы и отзывы пользователей.';
+
+  @override
+  String get filter => 'Как использовать фильтры?';
+
+  @override
+  String get filter_answer => 'Используйте фильтры, чтобы настроить поиск по стране, городу, типу процедуры, рейтингу, цене и отзывам.';
+
+  @override
+  String get incorrect => 'Что делать при ошибках в приложении?';
+
+  @override
+  String get incorrect_answer => 'Если у вас есть вопросы или проблемы, свяжитесь с нами через контактную форму или по электронной почте, указанной на странице.';
 }
